f74f64cc3883929f80ccb50b9907eb63d2b40052
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-24 10:57:49 -0700

    add test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -11,5 +11,29 @@ RSpec.describe Author do
     it 'exits' do
       expect(@charlotte_bronte).to be_an_instance_of Author
     end
+
+    it 'has a name' do
+      expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
+    end
+
+    it 'has empty array of books' do
+      expect(@charlotte_bronte.books).to eq([])
+    end
+
+    it 'adds another book' do
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      expect(@jane_eyre.class).to eq(Book)
+    end
+
+    it 'has a title' do
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      expect(@jane_eyre.title).to eq("Jane Eyre")
+    end
+
+    it 'adds another book' do
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      @villette = @charlotte_bronte.write("Villette", "1853")
+      expect(@charlotte_bronte.books).to eq(@jane_eyre, @villette)
+    end
   end
 end
