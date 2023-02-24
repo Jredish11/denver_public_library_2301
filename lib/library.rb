1ce38d6528970ed02888f8388e62a0d409483a19
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-24 10:57:31 -0700

    add class

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,21 @@
 class Library
- 
+  attr_reader :name, :books, :authors
+  def initialize
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @author << author
+  end
+
+  def publication_time_frame_for(author)
+    hash = {}
+    hash[:start] = author.publication_date
+    hash[:end] = author.publication
+  end
+
+
 end
+ 
