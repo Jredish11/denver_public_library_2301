11414bb799ab7aba646fdb43b404bcec5158e6b3
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-24 09:22:42 -0700

    feat: Add methods for pub year and author

--- lib/book.rb
+++ lib/book.rb
@@ -8,5 +8,13 @@ class Book
   def title
     @hash[:title]
   end
+
+  def author
+    @hash[:author_first_name] + " " + @hash[:author_last_name]
+  end
+
+  def publication_year
+    @hash[:publication_date][9..12]
+  end
   
 end
