93d430f0206ce480c85aae64e6782e128482ebb9
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-24 10:57:07 -0700

    add methods

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,20 @@
 class Author 
-
+  
+  attr_reader :books
   def initialize(hash)
     @hash = hash
-    
+    @books = []
   end
+
+  def name
+    @hash[:first_name] + " " + @hash[:last_name]
+  end
+
+  def write(title, publication_date)
+    book_new = Book.new({author_first_name: @hash[:first_name], author_last_name: @hash[:last_name], title: title, publication_date: publication_date})
+  end
+
+
+  
+
 end
